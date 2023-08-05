59caf9fb5a42476bbcda2a659f49befa2df1678b
priyam9669 <priyamkumardas@gmail.com>
2023-08-05 10:42:31 +0530

    Initail Commit

---
+++ src/test/java/com/example/test2/test2_maven/Test2MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test2.test2_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2MavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
